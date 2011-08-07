88d99812c4df996e8d73845f81cfe455411c4d03
Lance Gatlin <lance.gatlin@gmail.com>
2011-08-07 14:21:26 -0400

    stable LocalSettings

--- html/w/LocalSettings.php
+++ html/w/LocalSettings.php
@@ -7,13 +7,16 @@
 # See includes/DefaultSettings.php for all configurable settings
 # and their default values, but don't forget to make changes in _this_
 # file, not there.
+#
+# Further documentation for configuration settings may be found at:
+# http://www.mediawiki.org/wiki/Manual:Configuration_settings
 
 # If you customize your file layout, set $IP to the directory that contains
 # the other MediaWiki files. It will be used as a base to locate files.
 if( defined( 'MW_INSTALL_PATH' ) ) {
-        $IP = MW_INSTALL_PATH;
+	$IP = MW_INSTALL_PATH;
 } else {
-        $IP = dirname( __FILE__ );
+	$IP = dirname( __FILE__ );
 }
 
 $path = array( $IP, "$IP/includes", "$IP/languages" );
@@ -21,50 +24,45 @@ set_include_path( implode( PATH_SEPARATOR, $path ) . PATH_SEPARATOR . get_includ
 
 require_once( "$IP/includes/DefaultSettings.php" );
 
-// cache type to try to stop depending on spotty mysql 
-//$wgParserCacheType = CACHE_NONE;
-
-# If PHP's memory limit is very low, some operations may fail.
-# ini_set( 'memory_limit', '20M' );
-
 if ( $wgCommandLineMode ) {
-        if ( isset( $_SERVER ) && array_key_exists( 'REQUEST_METHOD', $_SERVER ) ) {
-                die( "This script must be run from the command line\n" );
-        }
+	if ( isset( $_SERVER ) && array_key_exists( 'REQUEST_METHOD', $_SERVER ) ) {
+		die( "This script must be run from the command line\n" );
+	}
 }
 ## Uncomment this to disable output compression
 # $wgDisableOutputCompression = true;
 
 $wgSitename         = "TI3Wiki.org";
 
-$wgLogo             = "http://www.ti3wiki.org/images/TI3logo.png";
-
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
-$wgScriptPath       = "";
+## For more information on customizing the URLs please see:
+## http://www.mediawiki.org/wiki/Manual:Short_URL
+$wgScriptPath       = "/w";
 $wgScriptExtension  = ".php";
 $wgArticlePath = "/wiki/$1";
 $wgUsePathInfo = true;
 
-## For more information on customizing the URLs please see:
-## http://www.mediawiki.org/wiki/Manual:Short_URL
+## The relative URL path to the skins directory
+$wgStylePath        = "$wgScriptPath/skins";
+
+## The relative URL path to the logo.  Make sure you change this from the default,
+## or else you'll overwrite your logo when you upgrade!
+$wgLogo             = "$wgStylePath/TI3/TI3logo.png";
+
+## UPO means: this is also a user preference option
 
 $wgEnableEmail      = true;
-$wgEnableUserEmail  = true;
+$wgEnableUserEmail  = true; # UPO
 
-$wgEmergencyContact = "lance.gatlin@yahoo.com";
+$wgEmergencyContact = "admin@ti3wiki.org";
 $wgPasswordSender = "admin@ti3wiki.org";
 
-## For a detailed description of the following switches see
-## http://www.mediawiki.org/wiki/Extension:Email_notification 
-## and http://www.mediawiki.org/wiki/Extension:Email_notification
-## There are many more options for fine tuning available see
-## /includes/DefaultSettings.php
-## UPO means: this is also a user preference option
 $wgEnotifUserTalk = true; # UPO
 $wgEnotifWatchlist = true; # UPO
 $wgEmailAuthentication = true;
 
+## Database settings
 $wgDBtype           = "mysql";
 require_once('LocalSettings.db.pwd');
 
@@ -72,23 +70,23 @@ require_once('LocalSettings.db.pwd');
 $wgDBprefix         = "";
 
 # MySQL table options to use during installation or update
-$wgDBTableOptions   = "TYPE=InnoDB";
+$wgDBTableOptions   = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
 
 # Experimental charset support for MySQL 4.1/5.0.
-$wgDBmysql5 = false;
-
-# Postgres specific settings
-$wgDBport           = "5432";
-$wgDBmwschema       = "mediawiki";
-$wgDBts2schema      = "public";
+$wgDBmysql5 = true;
 
 ## Shared memory settings
 $wgMainCacheType = CACHE_ACCEL;
 $wgMemCachedServers = array();
 
+# File cache
+$wgUseFileCache = true; /* default: false */ 
+$wgFileCacheDirectory = "$IP/cache"; 
+$wgDisableCounters = true; 
+
 ## To enable image uploads, make sure the 'images' directory
 ## is writable, then set this to true:
-$wgEnableUploads = true;
+$wgEnableUploads       = true;
 $wgUseImageResize = true;
 $wgUseImageMagick = true;
 $wgImageMagickConvertCommand = "/usr/bin/convert";
@@ -97,6 +95,14 @@ $wgAllowCopyUploads = true;
 $wgMaxShellMemory = 524288;
 $wgUploadSizeWarning = 1024 * 1024 * 50;
 $wgUploadMaxUploadSize = 1024 * 1024 * 150;
+$wgFileExtensions = array( 'bmp', 'png', 'gif', 'jpg', 'jpeg', 'wav', 'pdf', 'doc', 'xls', 'ppt', 'zip' );
+$wgMimeDetectorCommand = "file -bi";
+$wgAllowExternalImages = true;
+
+## If you use ImageMagick (or any other shell command) on a
+## Linux server, this will need to be set to the name of an
+## available UTF-8 locale
+$wgShellLocale = "en_US.utf8";
 
 ## If you want to use image uploads under safe mode,
 ## create the directories images/archive, images/thumb and
@@ -108,15 +114,19 @@ $wgUploadMaxUploadSize = 1024 * 1024 * 150;
 ## you can enable inline LaTeX equations:
 $wgUseTeX           = false;
 
-$wgLocalInterwiki   = $wgSitename;
+## Set $wgCacheDirectory to a writable directory on the web server
+## to make your wiki go slightly faster. The directory should not
+## be publically accessible from the web.
+#$wgCacheDirectory = "$IP/cache";
+
+$wgLocalInterwiki   = strtolower( $wgSitename );
 
 $wgLanguageCode = "en";
 
-$wgProxyKey = "204a68c0fde054cd9089f9b935420a1f8da4615bd69634bc48eb078aa3ac5170";
-
 ## Default skin: you can change the default skin. Use the internal symbolic
-## names, ie 'standard', 'nostalgia', 'cologneblue', 'monobook':
-$wgDefaultSkin = 'monobook';
+## names, ie 'vector', 'monobook':
+#$wgDefaultSkin = 'vector';
+$wgDefaultSkin = 'ti3';
 
 ## For attaching licensing metadata to pages, and displaying an
 ## appropriate copyright notice / icon. GNU Free Documentation
@@ -124,69 +134,62 @@ $wgDefaultSkin = 'monobook';
 $wgEnableCreativeCommonsRdf = true;
 $wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
 $wgRightsUrl = "http://www.gnu.org/copyleft/fdl.html";
-$wgRightsText = "GNU Free Documentation License 1.2";
+$wgRightsText = "GNU Free Documentation License 1.3";
 $wgRightsIcon = "${wgScriptPath}/skins/common/images/gnu-fdl.png";
-# $wgRightsCode = "gfdl"; # Not yet used
+# $wgRightsCode = "gfdl1_3"; # Not yet used
 
 $wgDiff3 = "/usr/bin/diff3";
 
 # When you make changes to this configuration file, this will make
 # sure that cached pages are cleared.
-$configdate = gmdate( 'YmdHis', @filemtime( __FILE__ ) );
-$wgCacheEpoch = max( $wgCacheEpoch, $configdate );
-$wgFileExtensions = array( 'bmp', 'png', 'gif', 'jpg', 'jpeg', 'wav', 'pdf', 'doc', 'xls', 'ppt', 'zip' );
-$wgMimeDetectorCommand = "file -bi";
+$wgCacheEpoch = max( $wgCacheEpoch, gmdate( 'YmdHis', @filemtime( __FILE__ ) ) );
 
-$wgUseFileCache = true; /* default: false */ 
-$wgFileCacheDirectory = "$IP/cache"; 
+# Prevent anon users from editing
+$wgGroupPermissions['*']['edit'] = false; 
+# For non-logged in users; Removes link to talk page for for non-logged in users
 $wgShowIPinHeader = false; 
-$wgDisableCounters = true; 
-
-$wgExtraNamespace = array();
-
-$wgGroupPermissions['*']['edit'] = false; # Prevent anon users from editting
-$wgShowIPinHeader = false; # For non-logged in users; Removes link to talk page for for non-logged in users
+# Require confirmed email to edit
 $wgEmailConfirmToEdit = true;
-$wgAllowExternalImages = true;
 
 $wgEnableScaryTranscluding = true;
 
-$wgShowExceptionDetails = true;
-
-$wgReadOnly = true;
-
-# START DEBUG SETTINGS
-
+###### START DEBUG SETTINGS #####
+/*
 $wgShowExceptionDetails = true;
 $wgShowSQLErrors = true;
 $wgDebugDumpSql  = true;
 $wgDebugLogFile ='ti3wiki.log';
 
-/** Only record profiling info for pages that took longer than this */
-#$wgProfileLimit = 0.0;
-/** Don't put non-profiling info into log file */
-#$wgProfileOnly = false;
-/** Log sums from profiling into "profiling" table in db. */
-#$wgProfileToDatabase = false;
-/** If true, print a raw call tree instead of per-function report */
-#$wgProfileCallTree = false;
-/** Should application server host be put into profiling table */
-#$wgProfilePerHost = false;
+# Only record profiling info for pages that took longer than this
+$wgProfileLimit = 0.0;
+# Don't put non-profiling info into log file
+$wgProfileOnly = false;
+# Log sums from profiling into "profiling" table in db. 
+$wgProfileToDatabase = false;
+# If true, print a raw call tree instead of per-function report
+$wgProfileCallTree = false;
+#Should application server host be put into profiling table
+$wgProfilePerHost = false;
 
-/** Settings for UDP profiler */
-#$wgUDPProfilerHost = '127.0.0.1';
-#$wgUDPProfilerPort = '3811';
+#Settings for UDP profiler 
+$wgUDPProfilerHost = '127.0.0.1';
+$wgUDPProfilerPort = '3811';
 
-/** Detects non-matching wfProfileIn/wfProfileOut calls */
-#$wgDebugProfiling = false;
-/** Output debug message on every wfProfileIn/wfProfileOut */
-#$wgDebugFunctionEntry = 0;
-/** Lots of debugging output from SquidUpdate.php */
-#$wgDebugSquid = false;
+# Detects non-matching wfProfileIn/wfProfileOut calls
+$wgDebugProfiling = false;
+# Output debug message on every wfProfileIn/wfProfileOut
+$wgDebugFunctionEntry = 0;
+# Lots of debugging output from SquidUpdate.php
+$wgDebugSquid = false;
+*/
 
+######## START MAINTENANCE SETTINGS ########
+/*
+# Make wiki read only
+$wgReadOnly = true;
 # Disable account creation
 $wgGroupPermissions['*']['createaccount'] = false;
 $wgGroupPermissions['sysop']['createaccount'] = true;
+*/
 
-require_once( 'extensions/extensions.php' );
-?>
+require_once('extensions/extensions.php');
